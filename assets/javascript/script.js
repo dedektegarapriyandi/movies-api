e6067d39064d2e630d5f7063592277444b9abfb0
dedektegarapriyandi <dedektegarapriyandi@gmail.com>
2021-10-29 19:20:49 +0700

    showing data api to card

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,14 +3,32 @@ searchBtn.addEventListener("click", async function () {
     const searchInput = document.querySelector(".search-input").value;
 
     const movies = await getAPI(searchInput);
-    movies.forEach(movie => {
-        console.log(movie);
-    });
+    moviesCard(movies);
 });
 
-const getAPI = function (searchInput) {
-    return fetch("http://www.omdbapi.com/?apikey=666c83eb&s=" + searchInput)
-        .then(response => response.json())
-        .then(data => data.Search);
+const getAPI = async function (searchInput) {
+    const response = await fetch("http://www.omdbapi.com/?apikey=666c83eb&s=" + searchInput);
+    const data = await response.json();
+    return data.Search;
 }
 
+const moviesCard = function (movies) {
+    const cardContainer = document.querySelector(".card-container");
+    let cards = '';
+    movies.forEach((movie) => {
+        cards += `<div class="col-md-4 my-4">
+                        <div class="card">
+                            <img src="${movie.Poster}" class="card-img-top" alt="poster">
+                            <div class="card-body">
+                                <h5 class="card-title">${movie.Title}</h5>
+                                <p class="card-text">${movie.Year}</p>
+                                <p class="card-text">${movie.Type}</p>
+                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                    Details
+                                </button>
+                            </div>
+                        </div>
+                    </div>`;
+    });
+    cardContainer.innerHTML = cards;
+}
